d0aebcaf3a760374ef8b04eaf6fa36fef62ebf34
Lidia Freitas <lidia.freitas@inlog.com.br>
2019-01-16 10:18:30 -0200

    change types for props lat/long

--- src/models/features/marker/marker-alter-options.ts
+++ src/models/features/marker/marker-alter-options.ts
@@ -2,11 +2,11 @@ import CircleMarkerStyle from './circle-marker-style';
 import MarkerIcon from './marker-icon';
 
 export default class MarkerAlterOptions {
-    public latlng?: number;
+    public latlng?: number[];
     public icon?: MarkerIcon;
     public style?: CircleMarkerStyle;
 
-    constructor(latlng?: number, icon?: MarkerIcon, style?: CircleMarkerStyle) {
+    constructor(latlng?: number[], icon?: MarkerIcon, style?: CircleMarkerStyle) {
         this.latlng = latlng;
         this.icon = icon;
         this.style = style;
